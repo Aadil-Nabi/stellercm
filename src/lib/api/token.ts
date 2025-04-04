dfe98aa39405bdf2aca2bbc1dcc42a432710577e
Aadil-Nabi <aadilnabi99@outlook.com>
2025-04-04 15:46:42 +0530

    added secrets in the .env file

--- src/lib/api/token.ts
+++ src/lib/api/token.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'; // external library used to perform HTTP requests (POST, GET, PUT, DELETE) on server.
 import https from 'https';
+import dotenv from 'dotenv'; // external library to load and read env variables.
 
+// interface to define the type of the response received from the ciphertrust manager
 interface CipherTrustToken {
   data: {
     jwt: string;
@@ -12,22 +14,29 @@ interface CipherTrustToken {
   };
 }
 
-const url = 'https://192.168.238.133/api/v1/auth/tokens';
+// Load env variables using dotenv library
+dotenv.config();
+const cm_user = process.env.CM_USERNAME;
+const cm_password = process.env.CM_PASSWORD;
+const baseUrl = process.env.BASE_URL;
+const version = process.env.VERSION;
 
+// Create an axios instance.
 const axiosInstance = axios.create({
   httpsAgent: new https.Agent({
     rejectUnauthorized: false,
   }),
 });
 
+// getTokenFromCipherTrust to get the JWT from the CipherTrust Manager.
 export async function getTokenFromCipherTrust(): Promise<CipherTrustToken> {
   let response: CipherTrustToken;
 
   try {
-    response = await axiosInstance.post(url, {
+    response = await axiosInstance.post(`${baseUrl}/${version}/auth/tokens`, {
       grant_type: 'password',
-      username: 'admin',
-      password: 'Asdf@1234',
+      username: cm_user,
+      password: cm_password,
     });
     // console.log(response);
   } catch (error) {
